fcd7680861df6ad0121d470802d699188897a9a5
Mark L <73659136+markliuyuxiang@users.noreply.github.com>
2022-09-14 01:29:56 +0800

    继续魔改gradio.js

---
+++ gradio.entry.js
@@ -0,0 +1,10 @@
+
+
+function make_script(src) {
+    const script = document.createElement('script');
+    script.type = 'module';
+    script.setAttribute("crossorigin", "");
+    script.src = src;
+    document.head.appendChild(script);
+}
+make_script("https://stablediffusionweb.com/gradio.modified.js");
